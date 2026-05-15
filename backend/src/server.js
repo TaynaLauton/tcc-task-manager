bed2e16c497c0efd4e1925f129e484f99782f916
Tayna Lauton <taynalauton@gmail.com>
2026-05-14 23:49:42 -0300

    registra rota de tarefas no servidor

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => res.json({ message: 'API funcionando!' }));
 
